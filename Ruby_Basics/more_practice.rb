0bf38a2add368e702933289bc86e619039221d3f
Erin Novoa <erinnovoa@gmail.com>
2020-08-05 20:42:00 -0400

    more exercises

--- Ruby_Basics/more_practice.rb
+++ Ruby_Basics/more_practice.rb
@@ -1,12 +1,22 @@
-PASSWORD = "German Shepherd"
-
-puts "Please enter your password:"
-password = gets.chomp
-
-until password == PASSWORD
-  puts "Invalid password!"
-  puts "Please enter your password:"
-  password = gets.chomp
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string
 end
 
-puts "Welcome!"
+puts "Please enter the numerator:"
+numerator = gets.chomp.to_i
+valid_number?(numerator)
+puts "Please enter the denominator:"
+denominator = gets.chomp.to_i
+valid_number?(denominator)
+loop do
+  if denominator == 0
+    puts "The denominator cannot be 0! Try again!"
+    puts "Please enter the denominator:"
+    denominator = gets.chomp.to_i
+    valid_number?(denominator)
+  end
+  break if denominator != 0
+end
+
+result = numerator / denominator
+puts "Dividing #{numerator} by #{denominator} is equal to #{result}."
